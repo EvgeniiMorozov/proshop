24ba4c8615a6f4728334cba73dc87d32c6a76400
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-04-08 12:41:14 +0500

    bugfix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from './screens/ProductScreen';
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
@@ -12,8 +12,10 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact />
-          <Route path="/product/:id" component={ProductScreen} />
+          <Routes>
+            <Route path="/" component={<HomeScreen/>} exact />
+            <Route path="/product/:id" component={<ProductScreen/>} />
+          </Routes>
         </Container>
       </main>
       <Footer />
