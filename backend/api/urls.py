c1ec6527aeb53cf252c5af58433cbfc47c70986a
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-04-13 10:47:34 +0500

    added get_products view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
-from unicodedata import name
 from django.urls import path
-from .views import get_routes
+from .views import get_routes, get_products
 
 
 urlpatterns = [
-    path("", get_routes, name="Route")
+    path("", get_routes, name="Route"),
+    path("products/", get_products, name="Products"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from .products_dev import products
+
 
 def get_routes(request):
     return JsonResponse("Hello", safe=False)
+
+
+def get_products(request):
+    return JsonResponse(products, safe=False)
