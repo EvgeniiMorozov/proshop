a07e6628a7b6b6bb37dedadf7b662a5702a9485e
EvgeniiMorozov <emorozov1101@gmail.com>
2022-05-09 17:30:48 +0500

    Updated footer and header components

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Container, Navbar, Nav, Row } from "react-bootstrap";
 
 function header() {
   return (
-    <div>
-      <header>Header</header>
-    </div>
-  )
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>
+                &nbsp;Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>
+                &nbsp;Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
-export default header
+export default header;
