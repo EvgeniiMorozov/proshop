bb5ae23740ae832e01e469f4f6a20fce059372bf
EvgeniiMorozov <emorozov1101@gmail.com>
2022-05-10 14:32:26 +0500

    React-router-dom connected to the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
