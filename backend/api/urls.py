af57da89d4cc29186ff73bc5898a1ecc840db48a
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-05-19 16:47:51 +0500

    Added jwt

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .views import get_routes, get_products, get_product
 
 
 urlpatterns = [
     path("", get_routes, name="Route"),
+    path("users/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("products/", get_products, name="Products"),
     path("products/<str:pk>/", get_product, name="Product"),
 ]
