e71e1a406b24039e39441627c8d20d956bef989e
EvgeniiMorozov <emorozov1101@gmail.com>
2022-05-22 11:42:02 +0500

    Added user-login-screen and functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
             <Route path="/products/:id" element={<ProductScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
           </Routes>
