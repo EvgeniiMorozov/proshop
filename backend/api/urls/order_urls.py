9d040106b4f0cfaa6cd4ea20984f7453254bdb73
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-05-27 13:48:44 +0500

    Added pay endpoint

--- backend/api/urls/order_urls.py
+++ backend/api/urls/order_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from api.views.order_views import add_order_items, get_order_by_id
+from api.views.order_views import add_order_items, get_order_by_id, update_order_to_paid
 
 urlpatterns = [
     path("add/", add_order_items, name="orders-add"),
     path("<str:pk>/", get_order_by_id, name="user-order"),
+    path("<str:pk>/pay/", update_order_to_paid, name="pay"),
 ]
--- backend/api/views/order_views.py
+++ backend/api/views/order_views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
@@ -73,3 +75,13 @@ def get_order_by_id(request, pk):
             {"detail": "Order does not exist"},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def update_order_to_paid(request, pk):
+    order = Order.objects.get(_id=pk)
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response("Order was paid")
