c8b74146d0584fcb0297289bb57694064a1a3cf1
EvgeniiMorozov <emorozov1101@gmail.com>
2022-05-27 20:52:39 +0500

    Added user orders view and url

--- backend/api/urls/order_urls.py
+++ backend/api/urls/order_urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from api.views.order_views import add_order_items, get_order_by_id, update_order_to_paid
+from api.views.order_views import (
+    add_order_items,
+    get_order_by_id,
+    get_user_orders,
+    update_order_to_paid,
+)
 
 urlpatterns = [
     path("add/", add_order_items, name="orders-add"),
+    path("my-orders/", get_user_orders, name="my-orders"),
     path("<str:pk>/", get_order_by_id, name="user-order"),
     path("<str:pk>/pay/", update_order_to_paid, name="pay"),
 ]
--- backend/api/views/order_views.py
+++ backend/api/views/order_views.py
@@ -85,3 +85,12 @@ def update_order_to_paid(request, pk):
     order.paidAt = datetime.now()
     order.save()
     return Response("Order was paid")
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_user_orders(request):
+    user = request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
