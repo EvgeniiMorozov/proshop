63a6e395df74bf62fe48a150cdb97b42f67b7ad2
EvgeniiMorozov <emorozov1101@gmail.com>
2022-05-28 09:13:39 +0500

    Added user list reducer, action and screen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -47,6 +47,22 @@ function header() {
                   </Nav.Link>
                 </LinkContainer>
               )}
+
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="admin-menu">
+                  <LinkContainer to="/admin/user-list">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <LinkContainer to="/admin/product-list">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <LinkContainer to="/admin/order-list">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
