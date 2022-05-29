b8f9a9049d0e87c32004673a7d2e74908595b28e
EvgeniiMorozov <emorozov1101@gmail.com>
2022-05-29 19:58:10 +0500

    Added create and update products endpoints

--- backend/api/urls/product_urls.py
+++ backend/api/urls/product_urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from api.views.product_views import delete_product, get_product, get_products
+from api.views.product_views import (
+    create_product,
+    delete_product,
+    get_product,
+    get_products,
+)
 
 urlpatterns = [
     path("", get_products, name="products"),
     path("<str:pk>/", get_product, name="product"),
+    path("create/", create_product, name="product-create"),
+    path("update/<str:pk>/", delete_product, name="product-update"),
     path("delete/<str:pk>/", delete_product, name="product-delete"),
 ]
