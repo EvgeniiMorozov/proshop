3bf94e23ba71dec93391f57eea2a455596de1a23
EvgeniiMorozov <emorozov1101@gmail.com>
2022-05-30 19:57:41 +0500

    Migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    rating = models.DecimalField(max_digits=7, decimal_places=2)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     countInStock = models.IntegerField(null=True, blank=True, default=0)
