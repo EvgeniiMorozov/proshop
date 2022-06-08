bf05512cef63a33fdf70144db45f85c28767ce76
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-06-08 10:42:21 +0500

    Added HashRouter

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -24,7 +24,7 @@ function header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <SearchBox />
-            <Nav className="me-auto">
+            <Nav className="ms-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i>
